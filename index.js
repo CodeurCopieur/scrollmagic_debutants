225e490e2139227b97aaeaa02df7520bfe1a40da
codwerk <louis_widdy@live.fr>
2020-10-17 23:23:57 +0200

    boucle pour chaque élément .project, acceder aux children de le l'élément .projet

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ $(document).ready(function () {
   //build a scene
   var ourScene = new ScrollMagic.Scene({
     triggerElement: '#projet-01 .div-rond',
-    duration: '60%',
-    triggerHook: .5
+    //le point final qui va arrêter l'animation
+    //duration: '60%',
+    //le point départ qui va declencher l'animation
+    triggerHook: .9,
+    reverse: false
   })
   //ajouter la class 'fade-in' sur #projet-01
   .setClassToggle('#projet-01', 'fade-in')
@@ -19,4 +22,27 @@ $(document).ready(function () {
     colorEnd: 'pink'
   })
   .addTo(controller);
+
+  //boucle pour chaque élément .project
+  $('.projet').each( function(){
+
+    //build a scene
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0],
+      //le point final qui va arrêter l'animation
+      duration: '100%',
+      //le point départ qui va declencher l'animation
+      triggerHook: .5
+    })
+    //ajouter la class 'fade-in' sur #projet-01
+    .setClassToggle(this, 'fade-in')
+    .addIndicators({
+      name: 'each scene',
+      colorTrigger: 'black',
+      //indent: 200,
+      colorStart: '#75C695',
+      colorEnd: 'pink'
+    })
+    .addTo(controller);
+  })
 })
