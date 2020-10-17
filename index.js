86984287b1bedfb068c9d53c0ec7c9af3c43e7f4
codwerk <louis_widdy@live.fr>
2020-10-17 19:33:21 +0200

    Utilisation des propriétés duration, triggerHook, colorStart, colorEnd

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ $(document).ready(function () {
   
   //build a scene
   var ourScene = new ScrollMagic.Scene({
-    triggerElement: '#projet-01'
+    triggerElement: '#projet-01 .div-rond',
+    duration: '60%',
+    triggerHook: .5
   })
   //ajouter la class 'fade-in' sur #projet-01
   .setClassToggle('#projet-01', 'fade-in')
   .addIndicators({
     name: 'fade scene',
     colorTrigger: 'black',
-    indent: 200,
-    colorStart: '#75C695'
+    //indent: 200,
+    colorStart: '#75C695',
+    colorEnd: 'pink'
   })
   .addTo(controller);
 })
