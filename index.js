1c725eea67b3f781eefb182e19376e056997e615
codwerk <louis_widdy@live.fr>
2020-10-18 20:58:01 +0200

    simple effet parallax

--- index.js
+++ index.js
@@ -21,7 +21,19 @@ $(document).ready(function () {
   .setPin('#intro', {pushFollowers: false})
   .addTo(controller);
 
+  var parallaxTl = new TimelineMax();
+  parallaxTl
+    .from('.content-wrapper', .4, {autoAlpha: 0, ease:Power0.easeNone}, .4)
+    .from('.bcg', 2, {y: '-50%', ease:Power0.easeNone}, 0);
 
+  //Parallax scene
+  var sliderParallaxScene = new ScrollMagic.Scene({
+    triggerElement: '.bcg-parallax',
+    triggerHook: 1,
+    duration: '200%'
+  })
+  .setTween(parallaxTl)
+  .addTo(controller);
 
   //build a scene
   var ourScene = new ScrollMagic.Scene({
